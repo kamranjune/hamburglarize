83c6766ee2881528859c9ac7f30fa5acddbd7aed
kamranjune <khsd6920@gmail.com>
2019-01-11 21:42:06 -0800

    use_env_variable JAWSDB_URL

--- config/config - Copy.js
+++ config/config - Copy.js
@@ -16,7 +16,10 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "JAWSDB_URL", 
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 };
